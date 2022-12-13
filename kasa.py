716c19cc4db1295a4b435adc6436901220373e0e
jpm021990 <120419263+jpm021990@users.noreply.github.com>
2022-12-13 11:44:55 -0500

    Update kasa.py

--- kasa.py
+++ kasa.py
@@ -4,7 +4,7 @@ from kasa import SmartPlug
 async def main():
     plug = SmartPlug("127.0.0.1")
 
-    await plug.update() "get latest state"
+    await plug.update() #(get latest state)
 
     if plug.is_on:
       await plug.turn_off()
