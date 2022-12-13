575ffa5c75f5608f5da39b63f87171e2dc341f21
jpm021990 <120419263+jpm021990@users.noreply.github.com>
2022-12-13 11:40:39 -0500

    Create kasa.py

---
+++ kasa.py
@@ -0,0 +1,16 @@
+import asyncio
+from kasa import SmartPlug
+
+async def main():
+    plug = SmartPlug("127.0.0.1")
+
+    await plug.update() "get latest state"
+
+    if plug.is_on:
+      await plug.turn_off()
+
+  if plug.is_off:
+    await plug.turn_on()
+
+if __name__ == "__main__":
+    asyncio.run(main())
