a8af98f70418fb4530d0433ee49b2f4361e81004
jpm021990 <120419263+jpm021990@users.noreply.github.com>
2022-12-13 11:50:51 -0500

    Update kasa.py

--- kasa.py
+++ kasa.py
@@ -2,15 +2,15 @@ import asyncio
 from kasa import SmartPlug
 
 async def main():
-    plug = SmartPlug("127.0.0.1")
+    plug = SmartPlug("10.118.139.183")
 
     await plug.update() #(get latest state)
 
     if plug.is_on:
       await plug.turn_off()
 
-  if plug.is_off:
-    await plug.turn_on()
+    if plug.is_off:
+      await plug.turn_on()
 
-if __name__ == "__main__":
-    asyncio.run(main())
+    if __name__ == "__main__":
+      asyncio.run(main())
